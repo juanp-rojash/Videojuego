466820dcc0e38ac2b6aa62690b3541cef488dcc7
juanp-rojash <juanp.rojash@upb.edu.co>
2021-10-28 15:31:28 -0500

    Juego

---
+++ js/nivel1.js
@@ -0,0 +1,79 @@
+let iconos = []
+let selecciones = []
+
+generarTablero()
+
+function cargarIconos() {
+    iconos = [
+        '<i class="fa-brands fa-amazon"></i>',
+        '<i class="fa-brands fa-android"></i>',
+        '<i class="fa-brands fa-dropbox"></i>',
+        '<i class="fa-brands fa-evernote"></i>',
+        '<i class="fa-brands fa-facebook"></i>',
+        '<i class="fa-brands fa-github"></i>',
+        '<i class="fa-brands fa-google-drive"></i>',
+        '<i class="fa-brands fa-google-play"></i>',
+        '<i class="fa-brands fa-instagram"></i>',
+        '<i class="fa-brands fa-linkedin"></i>',
+        '<i class="fa-brands fa-reddit"></i>',
+        '<i class="fa-brands fa-spotify"></i>'
+    ]
+}
+
+function generarTablero() {
+    cargarIconos()
+    selecciones = []
+
+    let tablero = document.getElementById("tablero")
+    let tarjetas = []
+
+    for(let i=0; i<24; i++){
+        tarjetas.push(`
+        <div class="area-tarjeta" onclick="seleccionarTarjeta(${i})">
+            <div class="tarjeta" id="tarjeta${i}">
+                <div class="cara trasera" id="trasera${i}">
+                    ${iconos[0]}
+                </div>
+                <div class="cara superior">
+                    <i class="fa-solid fa-circle-question"></i>
+                </div>
+            </div>
+        </div>    
+        `)
+        if(i%2 == 1){
+            iconos.splice(0, 1)
+        }
+    }
+    tarjetas.sort(()=>Math.random()-0.5)
+    tablero.innerHTML = tarjetas.join(" ")
+}
+
+function seleccionarTarjeta(i) {
+    let tarjeta = document.getElementById("tarjeta"+i)
+
+    if(tarjeta.style.transform != "rotateY(180deg)"){
+        tarjeta.style.transform = "rotateY(180deg)"
+        selecciones.push(i)
+    }
+    if(selecciones.length == 2){
+       deseleccionar(selecciones) 
+       selecciones = []
+    }
+}
+
+function deseleccionar(selecciones) {
+    setTimeout(() => {
+        let trasera1 = document.getElementById("trasera" + selecciones[0])
+        let trasera2 = document.getElementById("trasera" + selecciones[1])
+        if(trasera1.innerHTML != trasera2.innerHTML){
+            let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
+            let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
+            tarjeta1.style.transform = "rotateY(0deg)"
+            tarjeta2.style.transform = "rotatey(0deg)"
+        }
+        else{
+            trasera1.style.background = "plum"
+            trasera2.style.background = "plum"
+        }
+    }, 1000);
+}
